fc826527ab6b29f87a06775e2294b8defcfdbd3f
Ahmed Refai <momo3956248@gmail.com>
2024-11-23 19:48:32 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+export default function App() {
+  return (
+    <>
+      <h1 className="bg-red-900 text-white">
+        <i className="fa-brands fa-facebook fa text-sm"></i>ahmed refai
+      </h1>
+    </>
+  );
+}
