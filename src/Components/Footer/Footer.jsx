88bbea70c1755a61183075a498886d418c8c5b5e
Ahmed Refai <momo3956248@gmail.com>
2024-11-23 23:16:09 +0200

    Design Home Page

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -4,12 +4,12 @@ export default function Footer() {
       <footer className="bg-firstprime text-white  relative">
         <div className="container grid grid-cols-12 gap-8 py-[90px]">
           <div className="section-left col-span-12 text-center sm:col-span-6 lg:col-span-4 space-y-2">
-            <h2 className="uppercase font-bold text-3xl">Location</h2>
+            <h3 className="uppercase font-bold text-3xl">Location</h3>
             <p>2215 John Daniel Drive</p>
             <p>Clark, MO 65243</p>
           </div>
           <div className="section-center col-span-12 text-center sm:col-span-6 lg:col-span-4 space-y-2">
-            <h2 className="uppercase font-bold text-3xl">AROUND THE WEB</h2>
+            <h3 className="uppercase font-bold text-3xl">AROUND THE WEB</h3>
             <ul className="flex gap-3 justify-center items-center text-xl">
               <li>
                 <a href="">
@@ -34,7 +34,7 @@ export default function Footer() {
             </ul>
           </div>
           <div className="section-right col-span-12 text-center sm:col-span-6 lg:col-span-4 space-y-2">
-            <h2 className="uppercase font-bold text-3xl">ABOUT FREELANCER</h2>
+            <h3 className="uppercase font-bold text-3xl">ABOUT FREELANCER</h3>
             <p>
               Freelance is a free to use, licensed Bootstrap theme created by
               Route
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,3 +1,30 @@
 export default function Home() {
-  return <div>Home</div>;
+  return (
+    <>
+      <section className="bg-secondprime text-white h-screen flex items-center justify-center">
+        <div className="container text-center space-y-4">
+          <h2 className="text-5xl font-semibold uppercase">about component</h2>
+          <div className="icon-star  flex items-center justify-center gap-3  ">
+            <div className="line"></div>
+            <i className="fa-solid fa-star text-xl "></i>
+            <div className="line"></div>
+          </div>
+          <div className="flex flex-col md:flex-row items-center justify-center gap-3 text-start">
+            <p>
+              Freelancer is a free bootstrap theme created by Route. The
+              download includes the complete source files including HTML, CSS,
+              and JavaScript as well as optional SASS stylesheets for easy
+              customization.
+            </p>
+            <p>
+              Freelancer is a free bootstrap theme created by Route. The
+              download includes the complete source files including HTML, CSS,
+              and JavaScript as well as optional SASS stylesheets for easy
+              customization.
+            </p>
+          </div>
+        </div>
+      </section>
+    </>
+  );
 }
