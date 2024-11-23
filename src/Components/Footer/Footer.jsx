0e3d767534cf345d4d80e2a11373978b603e7d1f
Ahmed Refai <momo3956248@gmail.com>
2024-11-23 21:40:52 +0200

    Design Footer

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,3 +1,50 @@
 export default function Footer() {
-  return <div>Footer</div>;
+  return (
+    <>
+      <footer className="bg-firstprime text-white  relative">
+        <div className="container grid grid-cols-12 gap-8 py-[90px]">
+          <div className="section-left col-span-12 text-center sm:col-span-6 lg:col-span-4 space-y-2">
+            <h2 className="uppercase font-bold text-3xl">Location</h2>
+            <p>2215 John Daniel Drive</p>
+            <p>Clark, MO 65243</p>
+          </div>
+          <div className="section-center col-span-12 text-center sm:col-span-6 lg:col-span-4 space-y-2">
+            <h2 className="uppercase font-bold text-3xl">AROUND THE WEB</h2>
+            <ul className="flex gap-3 justify-center items-center text-xl">
+              <li>
+                <a href="">
+                  <i className="fa-brands fa-facebook fa"></i>
+                </a>
+              </li>
+              <li>
+                <a href="">
+                  <i className="fa-brands fa-twitter"></i>
+                </a>
+              </li>
+              <li>
+                <a href="">
+                  <i className="fa-brands fa-linkedin-in"></i>
+                </a>
+              </li>
+              <li>
+                <a href="">
+                  <i className="fa-solid fa-globe"></i>
+                </a>
+              </li>
+            </ul>
+          </div>
+          <div className="section-right col-span-12 text-center sm:col-span-6 lg:col-span-4 space-y-2">
+            <h2 className="uppercase font-bold text-3xl">ABOUT FREELANCER</h2>
+            <p>
+              Freelance is a free to use, licensed Bootstrap theme created by
+              Route
+            </p>
+          </div>
+        </div>
+        <div className="bg-[#1A252F] text-center py-4">
+          Copyright © Your Website 2021
+        </div>
+      </footer>
+    </>
+  );
 }
