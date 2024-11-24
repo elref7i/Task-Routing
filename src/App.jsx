4ab8096b214bdbfef1cd710bbcd76d783a8ae190
Ahmed Refai <momo3956248@gmail.com>
2024-11-24 19:23:11 +0200

    update home

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import Layout from './Components/Layout/Layout';
-import Home from './Pages/Home/Home';
 import About from './Pages/About/About';
 import Portfolio from './Pages/Portfolio/Portfolio';
 import Contact from './Pages/Contact/Contact';
@@ -8,10 +7,9 @@ import Contact from './Pages/Contact/Contact';
 export default function App() {
   const router = createBrowserRouter([
     {
-      path: '/',
+      path: '/home',
       element: <Layout />,
       children: [
-        { path: '/home', element: <Home /> },
         { path: '/about', element: <About /> },
         { path: '/portfolio', element: <Portfolio /> },
         { path: '/contact', element: <Contact /> },
